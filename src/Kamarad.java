0df56a796fe1b2d938eaa6bccbd267ba9558408f
musond12 <musond12@gasos-ro.cz>
2025-02-17 13:03:51 +0100

    lol

---
+++ src/Kamarad.java
@@ -0,0 +1,16 @@
+public class Kamarad {
+    private String jmeno;
+    private int rokNarozeni;
+
+    public Kamarad(String jmeno, int rokNarozeni) {
+        this.jmeno = jmeno;
+        this.rokNarozeni = rokNarozeni;
+    }
+    public String getJmeno() {
+        return jmeno;
+    }
+    public int getRokNarozeni() {
+        return rokNarozeni;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        /*
+        //Úkol a
+        String [] pole = {"Kuba", "Štěpán", "Michal", "Jirka","Tomáš"};
+        for(int i=0; i<pole.length; i++){
+            System.out.print(pole[i]+"-");
+        }*/
+        //úkol b
+        /*
+        int N;
+        double[] znamky;
+        System.out.print("Napiš kolik čísel bude v poli: ");
+        N = sc.nextInt();
+        sc.nextLine();
+        znamky = new double[N];
+        for (int i = 0; i < N; i++) {
+            System.out.println("Zadej " + (i + 1) + " číslo");
+            znamky[i] = sc.nextDouble();
         }
+        System.out.println("Kolikátou známku bys chtěl znát?");
+        int cislo = sc.nextInt();
+        sc.nextLine();
+        if (cislo >0 && cislo <= N) {
+            System.out.print("Známka je: ");
+            System.out.print(znamky[cislo - 1]);
+        }
+        else {
+            System.out.println("Číslo musí být mezi 1-"+N);
+        }
+        */
+        //úkol c
+        Kamarad [] kamos = new Kamarad[3];
+        for (int i = 0; i < kamos.length; i++) {
+            System.out.println("Napiš jméno: ");
+            String jmeno = sc.nextLine();
+            System.out.println("Napiš rok narození: ");
+            int rok = sc.nextInt();
+            sc.nextLine();
+            kamos[i] = new Kamarad(jmeno,rok);
+        }
+        double min = Double.MAX_VALUE;
+
+
     }
 }
